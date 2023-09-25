2d46a5738189c0f6b277c303f93c92fa9306ae30
Marcelo Nazar <marcelorum@gmail.com>
2023-09-25 00:56:39 -0300

    Records

--- webapp/views.py
+++ webapp/views.py
@@ -48,3 +48,23 @@ def register_user(request):
 		return render(request, 'register.html', {'form':form})
 
 	return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+      if request.user.is_authenticated:
+            # Look up record
+            customer_record = Record.objects.get(id=pk)
+            return render(request, 'record.html', {'customer_record':customer_record})
+      else:
+            messages.success(request, "You must be logged in")
+            return redirect('home')
+      
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Records deleted successfully!")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged in")
+        return redirect('home')
+          
